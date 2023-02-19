74025407c1449fa6acd914c9e23b6e980c96c36c
Just Kah <Kahswdirect23@gmail.com>
2023-02-19 09:12:58 -0500

    house keeping

--- SQL Query Code/Average_Per_user.sql
+++ SQL Query Code/Average_Per_user.sql
@@ -1,8 +1,8 @@
 SELECT 
   Distinct Id,
   ## Finds the average per day and removes teh decimal
-  CAST(ROUND(AVG(Calories),0) AS int64) AS Calories_Per_day,
-  CAST(ROUND(AVG(TotalSteps),0) AS int64) AS Total_steps_Per_Day,
+  ROUND(AVG(Calories),0) AS Calories_Per_day,
+  ROUND(AVG(TotalSteps),0) AS Total_steps_Per_Day,
   ROUND(AVG(TotalDistance),2) AS Distance_traveled_Per_Day,
   ROUND(AVG(TrackerDistance),2) AS Distance_tracked_Per_Day
 
