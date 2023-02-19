6245393249f0107f61f095eaaadc927fb83017b9
Just Kah <Kahswdirect23@gmail.com>
2023-02-19 16:50:37 -0500

    correct plots to remove outliers

--- R_Script/Averages.R
+++ R_Script/Averages.R
@@ -2,19 +2,27 @@
 install.packages('rmarkdown')
 library(rmarkdown)
 install.packages("tidyverse")
-library(tidyverse)
-install.packages("ggplot2")
+library(tidyverse) 
+install.packages("ggplot2") 
 library(ggplot2)
+install.packages("dplyr")
+library(dplyr)
 
 #####  Calories   #################################
 
+
+
 # loads calories file
 library(readxl)
-calories <- read_excel("SQL Query Code/Results/Average Calories per day.xls")
-calo = data.frame(calories)
-
-# Makes scatter plot for the Average Calories per day
-ggplot(data=calo, mapping = aes(x=ActivityDate, y=Calories_Per_day)) +geom_point() +
+  calories <- read_excel("SQL Query Code/Results/Average Calories per day.xls")
+  calo = data.frame(calories) 
+  
+    
+  #Filters out the outlier
+  filt_calo = filter(calo, Calories_Per_day > 1900) 
+  
+  # Makes scatter plot for the Average Calories per day
+  ggplot(data= filt_calo, mapping = aes(x=ActivityDate, y=Calories_Per_day)) +geom_point() +
   geom_smooth(method = lm) + ggtitle('Average Calories Per Day') + labs(x = 'Day', y ='Calories')
 
 
@@ -26,8 +34,11 @@ library(readxl)
 tracked_distance <- read_excel("SQL Query Code/Results/Average Distance Tracked.xls")
 trac = data.frame(tracked_distance)
 
+#Filters out the outlier
+filt_trac = filter(trac, Distance_tracked_Per_Day > 2.5) 
+
 # Makes scatter plot for the Average Tracked Distance per day
-ggplot(data=trac, mapping = aes(x=ActivityDate, y=Distance_tracked_Per_Day)) +geom_point() +
+ggplot(data=filt_trac, mapping = aes(x=ActivityDate, y=Distance_tracked_Per_Day)) +geom_point() +
   geom_smooth(method = lm) + ggtitle('Average Tracked Distance Per Day') + labs(x = 'Day', y ='Distance')
 
 
@@ -40,9 +51,11 @@ library(readxl)
 distance <- read_excel("SQL Query Code/Results/Average Distance Traveled.xls")
 dis = data.frame(distance)
 
+#Filters out the outlier
+filt_dis= filter(dis, Distance_traveled_Per_Day > 2.5) 
 
 # Makes scatter plot for the Average Tracked Distance per day
-ggplot(data=dis, mapping = aes(x=ActivityDate, y=Distance_traveled_Per_Day)) +geom_point() +
+ggplot(data=filt_dis, mapping = aes(x=ActivityDate, y=Distance_traveled_Per_Day)) +geom_point() +
   geom_smooth(method = lm) + ggtitle('Average Distance Per Day') + labs(x = 'Day', y ='Distance')
 
 
@@ -55,8 +68,11 @@ library(readxl)
 steps <- read_excel("SQL Query Code/Results/Average steps per day.xls")
 ste = data.frame(steps)
 
+#Filters out the outlier
+filt_ste = filter(ste, Average_Total_steps > 2.5)
+
 # Makes scatter plot for the Average Tracked Distance per day
-ggplot(data=ste, mapping = aes(x=ActivityDate, y=Average_Total_steps)) +geom_point() +
+ggplot(data=filt_ste, mapping = aes(x=ActivityDate, y=Average_Total_steps)) +geom_point() +
   geom_smooth(method = lm) + ggtitle('Average Steps Per Day') + labs(x = 'Day', y ='Steps')
 
 
