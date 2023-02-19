27120e88c1890d4a831301e99d5d532d593f5661
Just Kah <Kahswdirect23@gmail.com>
2023-02-19 08:07:13 -0500

    added sql code

---
+++ SQL Query Code/Collect_All_Averages.sql
@@ -0,0 +1,12 @@
+SELECT 
+
+##finds the average callories burned
+  CAST(ROUND(AVG(Calories),0) AS int64) AS Average_Calories_burned,
+  CAST(ROUND(AVG(TotalSteps),0) AS int64) AS AVerage_Total_steps,
+  CAST(ROUND(AVG(TotalDistance),0) AS int64) AS Average_Distance_traveled,
+  CAST(ROUND(AVG(TrackerDistance),0) AS int64) AS Average_Distance_tracked
+
+## Note logged activity was not tracked because majority of the entered data is 0
+
+ FROM 
+  `bellabeat-trends.Full_activity.Activity` 
