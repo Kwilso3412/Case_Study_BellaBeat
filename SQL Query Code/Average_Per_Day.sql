f11db511e916f79a9a92703d7db132273e4aab65
Just Kah <Kahswdirect23@gmail.com>
2023-02-19 08:46:29 -0500

    added more sql Query and results

---
+++ SQL Query Code/Average_Per_Day.sql
@@ -0,0 +1,15 @@
+SELECT 
+  Distinct ActivityDate,
+  ## Finds the average per day and removes teh decimal
+  CAST(ROUND(AVG(Calories),0) AS int64) AS Calories_Per_day,
+  CAST(ROUND(AVG(TotalSteps),0) AS int64) AS Total_steps_Per_Day,
+  ROUND(AVG(TotalDistance),2) AS Distance_traveled_Per_Day,
+  ROUND(AVG(TrackerDistance),2) AS Distance_tracked_Per_Day
+
+ FROM
+   `bellabeat-trends.Full_activity.Activity` 
+
+GROUP BY 
+  ActivityDate
+ Order by 
+  ActivityDate 
--- SQL Query Code/Average_of_all_users.sql
+++ SQL Query Code/Average_of_all_users.sql
@@ -3,8 +3,8 @@ SELECT
 ##finds the average callories burned
   CAST(ROUND(AVG(Calories),0) AS int64) AS Average_Calories_burned,
   CAST(ROUND(AVG(TotalSteps),0) AS int64) AS AVerage_Total_steps,
-  CAST(ROUND(AVG(TotalDistance),0) AS int64) AS Average_Distance_traveled,
-  CAST(ROUND(AVG(TrackerDistance),0) AS int64) AS Average_Distance_tracked
+  ROUND(AVG(TotalDistance),0) AS Average_Distance_traveled,
+  ROUND(AVG(TrackerDistance),0) AS Average_Distance_tracked
 
 ## Note logged activity was not tracked because majority of the entered data is 0
 
